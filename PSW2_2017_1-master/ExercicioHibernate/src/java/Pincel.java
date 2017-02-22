d043b0e3df16bf33a10841f31e32876d767cee9b
pablixa <pablo.gsolano@live.com>
2017-02-21 22:19:57 -0300

    exercício da aula 21/02 leitura no banco de dados

---
+++ PSW2_2017_1-master/ExercicioHibernate/src/java/Pincel.java
@@ -0,0 +1,40 @@
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+@Entity
+public class Pincel {
+    private String cor;
+    private String fabricante;
+    @Id
+    private int num_serie;
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public int getNum_serie() {
+        return num_serie;
+    }
+
+    public void setNum_serie(int num_serie) {
+        this.num_serie = num_serie;
+    }
+    
+    
+    
+    
+}
